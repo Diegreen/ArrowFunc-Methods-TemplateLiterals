802551c33c48ba675f191d599c5e114450f7be3f
diego <diegonegocios21@outlook.com>
2023-06-13 22:54:57 -0300

    Complete

--- metodos.js
+++ metodos.js
@@ -1,3 +1,4 @@
+//map
 let users = ["Samuel", "Patrick", "Matheus", "Maria"]
 
 
@@ -7,9 +8,13 @@ const usersCurrent = users.map((element) => {
 
 console.log(usersCurrent)
 
+const numbers = [1, 2, 3, 4, 5];
+const doubleNumbers = numbers.map(function(number) {
+  return number * 2;
+});
+console.log(doubleNumbers);
 //users = users.toString() && users.join(' / ')
 //console.log(users)
-
 
 let employeeBonus = [12, 14, 16, 23]
 
@@ -19,21 +24,35 @@ const currentEmployeeBonus = employeeBonus.map((element) => {
 
 console.log(currentEmployeeBonus)
 console.log(employeeBonus)
-
+//filter
 let employeeBonus2 = [32, 34, 36, 33, 10, 9, 20, 35, 88, 55]
 
 const valuesLaterThanThirdy = employeeBonus2.filter((element) => {
     return element > 32 
 })
-
+//reduce
 const valuesReduce = employeeBonus2.reduce((previousValue, currentValue) => {
     return previousValue + currentValue 
 }, 10)
 console.log(valuesReduce)
 console.log(valuesLaterThanThirdy)
-
+//some
 const even = (element) => element % 2 === 0
 console.log(employeeBonus2.some(even))
 
+//find
 const found = employeeBonus2.find((element) => element > 31)
 console.log(found)
+
+const numbers2 = [1, 2, 3, 4, 5];
+const foundNumber = numbers2.find(number => number === 3);
+console.log(foundNumber);
+
+const persons = [
+    { name: 'John', age: 30 },
+    { name: 'Jane', age: 25 },
+    { name: 'Bob', age: 40 },
+    { name: 'Alice', age: 35 }
+  ];
+  const foundPerson = persons.find(person => person.age === 40);
+  console.log(foundPerson);
